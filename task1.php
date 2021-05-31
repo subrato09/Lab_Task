79eb128c7754fcbdb1d75011b1f8a7c7e224a0c5
subrato09 <85048839+subrato09@users.noreply.github.com>
2021-05-31 21:03:51 +0600

    Registration Form Created

---
+++ task1.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+ <head>
+      <title> Registration Form </title>
+</head>
+
+ <body>
+   <form>
+     <table>
+         <tr>
+             <td>Full Name: </td>
+             <td><input type="text"></td>
+         </tr>
+        <tr>
+             <td>E-mail: </td>
+             <td><input type="text"></td>
+         </tr>
+         <tr>
+             <td>Pasword:</td>
+             <td><input type="text"></td>
+         </tr>
+         </tr>
+         <tr>
+             <td>Comment:</td>
+             <td>
+             <textarea name="Shuvo" id="5" cols="40" row="5">
+             </textarea>
+             </td>
+         </tr>
+         <tr>
+             <td>Gender:</td>
+             <td>
+                 <input type="radio" name=Gender"> Male
+                 <input type="radio" name=Gender"> Female
+                 <input type="radio" name=Gender"> Others
+             </td>
+         </tr>
+          <tr>
+          <tr>
+             <td>Please choose a Hobby:</td>
+             <td>
+                 <input type="Checkbox" name=Hobby"> Singing
+                 <input type="Checkbox" name=hobby"> Dancing
+                 <input type="Checkbox" name=hobby"> Reading
+             </td>
+          </tr>
+          <tr>
+             <td>Please choose a file:</td>
+             <td>
+                 <input type="Submit" value="Choose File">
+             </td>
+          </tr>
+          <tr>
+             <td>
+                 <input type="Submit" value="Submit">
+                 <input type="Submit" value="Reset">
+             </td>
+          </tr>
+    </table>
+   </form>
+  </body>
+ </html>
